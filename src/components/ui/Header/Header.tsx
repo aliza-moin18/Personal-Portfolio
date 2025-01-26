eb8b453ac92777ae786d1db4c0ab5474cda82abb
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2025-01-26 00:30:20 -0800

    Update Header.tsx

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,80 +1,8 @@
-// "use client";
-
-// import Link from "next/link";
-// import { useState } from "react";
-// import { Button } from "../button";
-// import Image from "next/image";
-
-// const Header = () => {
-//   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-//   return (
-//     <header className="bg-black py-1 xl:py-5 text-white border-b border-gray-400">
-//       <div className="max-w-screen-xl mx-auto px-10 flex justify-between items-center">
-
-//         {/* Link with Logo Image */}
-//         <Link href="/" aria-label="Logo">
-//           <Image
-//             src="/logo.jpeg" 
-//             alt="Logo"
-//             width={115} 
-//             height={60}
-//             priority 
-//           />
-//         </Link>
-
-//         {/* Navbar (Desktop) */}
-//         <div className="hidden xl:flex items-center gap-7">
-//           <nav className="flex space-x-6">
-//             <Link href="/" className="text-white hover:text-purple-600">Home</Link>
-//             <Link href="/about" className="text-white hover:text-purple-600">About Me</Link>
-//             <Link href="/services" className="text-white hover:text-purple-600"> Services </Link>
-//             <Link href="/work" className="text-white hover:text-purple-600"> Work </Link>
-//             <Link href="/contact" className="text-white hover:text-purple-600">Contact</Link>
-//           </nav>
-//           <Link href="/contact">
-//             <Button className="text-white bg-[#BD59D3] border-[#9615DB] hover:border-[#932DAA] hover:bg-[#932DAA] transition-all">
-//               Hire me
-//             </Button>
-//           </Link>
-//         </div>
-
-//         {/* Mobile Nav Button */}
-//         <div className="xl:hidden items-center gap-8">
-//           <button
-//             className="text-white"
-//             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-//             aria-label={isMobileMenuOpen ? 'Close menu' : 'Open menu'}
-//           >
-//             {isMobileMenuOpen ? '✖️' : '☰'}
-//           </button>
-//         </div>
-
-//       </div>
-
-//       {/* Mobile Menu */}
-//       {isMobileMenuOpen && (
-//         <div className="md:hidden bg-black text-white p-4 space-y-2">
-//             <Link href="/" className="text-white hover:text-purple-600">Home</Link>
-//             <Link href="/about" className="text-white hover:text-purple-600">About Me</Link>
-//             <Link href="/services" className="text-white hover:text-purple-600"> Services </Link>
-//             <Link href="/work" className="text-white hover:text-purple-600"> Work </Link>
-//             <Link href="/contact" className="text-white hover:text-purple-600">Contact</Link>
-//         </div>
-//       )}
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
-
 "use client";
 
 import Link from "next/link";
 import { useState } from "react";
-import { Button } from "../button";
+import { Button } from "../ui/button";
 import Image from "next/image";
 
 const Header = () => {
